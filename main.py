64256c89e04cd7fcdfab453a0b5012ac4f90b70b
mykhailo <mikclemorgan@gmail.com>
2022-12-02 21:34:41 +0100

    Installed Flask and PyTelegramBotAPI in project, created Flask(__name__) init and putted TOKEN in global OS variables. Added in the end of code 2 functions for handling messages and testing especially for Heroku deploy

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
+from flask import Flask, request
 import telebot
-from telebot import types
+import os
 
-bot = telebot.TeleBot("5944424312:AAF3ttvABVk06IqEi993fpHodRaHyJE95lA")
+app = Flask(__name__)
+TOKEN = os.environ.get('TOKEN')
+bot = telebot.TeleBot(TOKEN)
+# bot = telebot.TeleBot("5944424312:AAF3ttvABVk06IqEi993fpHodRaHyJE95lA")
+# should be putted in global variables in OS on Heroku
 
 
 # Робимо меню
@@ -36,3 +41,18 @@ def info():
     pass
 bot.polling(none_stop=True)
 
+@app.route('/' + TOKEN, methods=['POST'])
+def get_message():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "Python Telegram Bot", 200
+
+
+@app.route('/')
+def main():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://multipurposechatbot.herokuapp.com/' + TOKEN)
+    return "Python Telegram Bot", 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
