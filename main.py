9e2559a61040e8afcbb4fb4372e366ddb4488152
mykhailo <mikclemorgan@gmail.com>
2022-12-03 02:50:14 +0100

    Added menu for left branch of the bot plan

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ bot = telebot.TeleBot(TOKEN)
 @bot.message_handler(commands=['start'])
 def start(message):
     """First main menu of bot"""
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
     info_Goloseev = telebot.types.KeyboardButton("Інформація про комплекс")
     school_Goloseev = telebot.types.KeyboardButton("Гірськолижна школа")
     rules_Goloseev = telebot.types.KeyboardButton("Правила")
@@ -36,6 +36,24 @@ def info_goloseev(message):
         markupInfo.add(info_gol, price_SkiPass, price_equipment, contacts)
         bot.send_message(message.chat.id, f"Радий бачити!", reply_markup=markupInfo)
 
+@bot.message_handler(content_types=['text'])
+def info_goloseev_options(message):
+    if message.text.lower().find('інфо') >= 0:
+        bot.send_message(message.chat.id, f"Загальна інформація про гірку!")
+    elif message.text.lower().find('skipass' or 'ski' or 'скай')  >= 0:
+        bot.send_message(message.chat.id, f"Ви получаете прайс лист SkiPass и правила")
+    elif message.text.lower().find('ціни' or 'цены' or 'price' or 'прайс') >= 0:
+        bot.send_document(message.chat.id, 'BQACAgIAAxkBAAEalgJjipEtMIwRF6HG09B05rgtLKE0JQAC8B4AAl2CWEhiYiIEvWrGwCsE' )
+    elif message.text.lower().find('контакты' or 'контакти' or 'contacts' or 'телефон') >= 0:
+        bot.send_message(message.chat.id, f"Ви в меню Контактной информации")
+    elif message.text.lower().find('маршрут' or 'location' or 'map'  or 'адрес') >= 0:
+        telebot.types.InlineKeyboardButton(text='Маршрут', url='https://g.page/GoloseevSkiPark')
+
+
+
+
+@bot.message_handler(content_types=['text'])
+def general_info(message):
 
 def info():
     pass
